4c22dee0191671cfa841ba5a07d73a18084e01c1
Charles Lee Lambert <c.lambert1213@gmail.com>
2018-03-30 14:13:08 -0700

    switched back to seperate init and config

--- init.el
+++ init.el
@@ -3,11 +3,12 @@
 ;;; Begin initialization
 ;; Turn off mouse interface early in startup to avoid momentary display
 
-(when window-system
+;; (when window-system
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
-  (tooltip-mode -1))
+(tooltip-mode -1)
+;; )
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
@@ -17,9 +18,9 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			   ("org"       . "http://orgmode.org/elpa/")
-			   ("melpa"     . "https://melpa.org/packages/")
-			   ("marmalade" . "http://marmalade-repo.org/packages/")
+                           ("org"       . "http://orgmode.org/elpa/")
+                           ("melpa"     . "https://melpa.org/packages/")
+                           ("marmalade" . "http://marmalade-repo.org/packages/")
                          )))
 (package-initialize) ; guess what this one does ?
 
@@ -36,8 +37,7 @@
 (use-package diminish
              :ensure t)
 
-;;; Load the config
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(load-file "~/.emacs.d/config.elc")
 
 (setq gc-cons-threshold 800000)
 (custom-set-variables
@@ -47,7 +47,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rjsx-mode company-tern anaconda-mode org-bullets evil-smartparens treemacs-evil seq-2\.14 which-key use-package spaceline solarized-theme mode-icons general flycheck evil-nerd-commenter evil-escape diminish counsel company))))
+    (esup yasnippet-snippets which-key use-package treemacs-projectile treemacs-evil telephone-line solarized-theme rjsx-mode prettier-js org-bullets git-gutter-fringe general flycheck evil-nerd-commenter evil-escape diminish counsel-projectile company-tern company-anaconda beacon))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
